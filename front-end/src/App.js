6a2ef36ff6213bf9eb2a9226317044cae9fd7ff1
mustafa <mustafa.sadriwala@utdallas.edu>
2021-02-05 13:06:50 -0600

    initial front-end commit

---
+++ front-end/src/App.js
@@ -0,0 +1,19 @@
+import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Jumbotron>
+        <Container>
+          <h1>And so it begins</h1>
+        </Container>
+      </Jumbotron>
+    </div>
+  );
+}
+
+export default App;
