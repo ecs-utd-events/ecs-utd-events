0582763f4448b76071e58ad4301dccab27376e14
mustafa <mustafa.sadriwala@utdallas.edu>
2021-02-09 19:02:33 -0600

    [FE] update Home page layout

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,7 +1,9 @@
 import Container from 'react-bootstrap/Container';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/esm/Col';
 import {
-    Link
+  Link
 } from "react-router-dom";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -10,6 +12,7 @@ import './../styles/App.css';
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
+import EventInfoCard from '../components/EventInfoCard';
 
 
 // Placeholder events for FullCalendar. Demonstrates creating events with unique ids.
@@ -17,53 +20,38 @@ let eventGuid = 0
 let todayStr = new Date().toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
 
 export const INITIAL_EVENTS = [
-    {
-      id: createEventId(),
-      title: 'All-day event',
-      start: todayStr
-    },
-    {
-      id: createEventId(),
-      title: 'Timed event',
-      start: todayStr + 'T12:00:00',
-      end: todayStr + 'T12:30:00'
-    }
+  {
+    id: createEventId(),
+    title: 'All-day event',
+    start: todayStr
+  },
+  {
+    id: createEventId(),
+    title: 'Timed event',
+    start: todayStr + 'T12:00:00',
+    end: todayStr + 'T12:30:00'
+  }
 ]
 
 export function createEventId() {
-    return String(eventGuid++)
+  return String(eventGuid++)
 }
 
 // Main page components
 export default function Home() {
-    return (
-        <div className="App">
-          <div className="HeaderDiv">
-            <Jumbotron>
-                <Container>
-                    <h1>Home Page</h1>
-                    <div>
-                        <Link to="/" style={{ padding: 10 }}>Home</Link>
-                        <Link to="/login">Organization Login</Link>
-                        <Link to="/org/create" style={{ padding: 10 }}>Create Event</Link>
-                    </div>
-                </Container>
-            </Jumbotron>
-          </div>
-          <div className="main-content">
-            <div className="main-page-sidebar">
-              <h2>Instructions</h2>
-              <ul>
-                <li>Select dates and you will be prompted to create a new event</li>
-                <li>Drag, drop, and resize events</li>
-                <li>Click an event to delete it</li>
-              </ul>
-            </div>
-
+  return (
+    <div className="App">
+      <div style={{paddingTop: "5rem"}}>
+        <Container fluid>
+        <Row>
+          <Col>
+            <EventInfoCard />
+          </Col>
+          <Col xs={9}>
             <div className="fullcalendar-div">
-              <FullCalendar 
+              <FullCalendar
                 defaultView="dayGridMonth"
-                plugins={[ dayGridPlugin, timeGridPlugin ]} 
+                plugins={[dayGridPlugin, timeGridPlugin]}
                 headerToolbar={{
                   left: 'prev,next today',
                   center: 'title',
@@ -73,8 +61,16 @@ export default function Home() {
                 height={'auto'}
               />
             </div>
-          </div>
+          </Col>
+        </Row>
+        </Container>
+        <div style={{padding: "5rem"}}>
+          <Link to="/" style={{ padding: 10 }}>Home</Link>
+          <Link to="/login">Organization Login</Link>
+          <Link to="/org/create" style={{ padding: 10 }}>Create Event</Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
