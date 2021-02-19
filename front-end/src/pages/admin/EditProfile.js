aa8935fa7fd3ffd9110f500b7e9a57551bfb1b21
mustafa <mustafa.sadriwala@utdallas.edu>
2021-02-19 10:54:28 -0600

    [FE] added admin portal routes

---
+++ front-end/src/pages/admin/EditProfile.js
@@ -0,0 +1,9 @@
+import { Container } from "react-bootstrap";
+
+export default function EditProfile() {
+    return (
+        <Container>
+            <h1>Edit Org Profile</h1>
+        </Container>
+    )
+}
