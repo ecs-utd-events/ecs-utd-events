1e7d1f93566548fc39930081d80dd5ccb190be02
sid-devic <sid.devic@gmail.com>
2021-02-18 22:50:52 -0600

    [FE] Int. changes from my manager Mustafa Sadriwala

--- front-end/src/components/NavbarComponent.js
+++ front-end/src/components/NavbarComponent.js
@@ -2,27 +2,12 @@ import { Navbar } from 'react-bootstrap'
 import { ReactComponent as ECSLogo } from '../assets/utd-ecs-logo-clipped.svg';
 
 export default function NavbarComponent({ page }) {
-    if (page == 'Home') {
-        return (
-            <Navbar className="mb-0 background">
-                <Navbar.Brand href="/">
-                    <ECSLogo height='8vh' />
-                </Navbar.Brand>
-            </Navbar>
-        );
-    }
-    else if (page == 'OrgProfilePage') {
-        return (
-            <Navbar className="mb-0 App">
-                <Navbar.Brand href="/">
-                    <ECSLogo height='8vh' />
-                </Navbar.Brand>
-            </Navbar>
-        );
-    }
-    else {
-        return (
-            null
-        );
-    }
+    const backgroundCSSName = page == 'OrgProfilePage' ? 'App' : 'background';
+    return (
+        <Navbar className={'mb-0 ' + backgroundCSSName}>
+            <Navbar.Brand href="/">
+                <ECSLogo height='8vh' />
+            </Navbar.Brand>
+        </Navbar>
+    );
 }
