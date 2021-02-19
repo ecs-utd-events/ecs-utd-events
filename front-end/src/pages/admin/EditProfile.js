762f6dc1abc528664ed21ab6845cc36e4bd96511
mustafa <mustafa.sadriwala@utdallas.edu>
2021-02-19 14:10:08 -0600

    [FE] added basic admin tabs on edit profile page

--- front-end/src/pages/admin/EditProfile.js
+++ front-end/src/pages/admin/EditProfile.js
@@ -1,9 +1,19 @@
-import { Container } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap/esm";
+import AdminTab from "../../components/AdminTab";
 
 export default function EditProfile() {
     return (
-        <Container>
-            <h1>Edit Org Profile</h1>
-        </Container>
+        <div className="background" style={{ minHeight: '100vh' }}>
+            <Container fluid>
+                <Row>
+                    <Col xs={3}>
+                        <AdminTab />
+                    </Col>
+                    <Col>
+                        <h1>Org Profile</h1>
+                    </Col>
+                </Row>
+            </Container>
+        </div >
     )
 }
