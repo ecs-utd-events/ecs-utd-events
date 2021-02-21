62717f064a4468b49a015b71004f1aa9c0cf4a60
mustafa <mustafa.sadriwala@utdallas.edu>
2021-02-21 12:00:34 -0600

    [FE] fix routing scroll issue

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,9 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  useLocation
 } from "react-router-dom";
+import { useEffect } from "react";
 
 import Login from './pages/Login';
 import Home from './pages/Home';
@@ -15,9 +17,21 @@ import HelpPage from "./pages/admin/HelpPage";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/App.css';
 
+
+export function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Switch>
         <Route path="/org" component={OrgProfileRouter} />
         <Route path="/login" component={Login} />
