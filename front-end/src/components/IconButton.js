f1513b3a387dbe9da99c770ba1b0b79c62964a77
mustafa <mustafa.sadriwala@utdallas.edu>
2021-02-20 21:13:10 -0600

    [FE] small updates to event info card

--- front-end/src/components/IconButton.js
+++ front-end/src/components/IconButton.js
@@ -1,11 +1,14 @@
- import Button from "react-bootstrap/esm/Button";
+import Button from "react-bootstrap/esm/Button";
 import './../styles/components.css';
+import { Icon } from '@iconify/react';
 
-export default function IconButton({ icon, ...props }) {
+export default function IconButton({ icon, SVGIcon, ...props }) {
+
     return (
         <Button bsPrefix={"icon-btn"} {...props}>
-                <span class="helper"></span>
-                <img className="icon" src={icon} />
+            <span class="helper"></span>
+            {icon != null && <Icon icon={icon} style={{ fontSize: '1.5rem' }} />}
+            {icon == null && SVGIcon != null && <img className="icon" src={SVGIcon} />}
         </Button>
     )
 }
