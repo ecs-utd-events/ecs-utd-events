501099a3052b0bbdf7f5ff03e422a00da59b7514
ShannenB <sbarrameda17@gmail.com>
2021-02-23 15:34:37 -0600

    [FE] switched to Form Controls

--- front-end/src/components/EditableEventCard.js
+++ front-end/src/components/EditableEventCard.js
@@ -1,119 +1,83 @@
-import { DialogTitle } from '@material-ui/core';
 import { useState, useRef } from 'react';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import { Editable } from './Editable';
+import { useForm } from 'react-hook-form';
+import Form from 'react-bootstrap/Form';
 
 export default function EditableEventCard({ event }) {
     const inputRef = useRef();
-    const [start, setStart] = useState(event.start);
-    const [end, setEnd] = useState(event.end);
+    const { register } = useForm();
+    const [dateTime, setDateTime] = useState(event.start);
     const [title, setTitle] = useState(event.title);
     const [location, setLocation] = useState(event.location);
     const [org, setOrg] = useState(event.org);
     const [link, setLink] = useState(event.link);
     const [description, setDescription] = useState(event.description);
-    
+
     if (event != null) {
         return (
-            <Card className="drop-shadow mb-4">
-                <Row>
-                    <Col xs={5} style={{ textAlign: 'left' }}>
-                        <h5>
-                        <Editable
-                                text={title}
+            <Form>
+                <Card className="drop-shadow mb-4">
+                    <Row>
+                        <Col xs={5} style={{ textAlign: 'left' }}>
+                            <h5>
+                                <Editable
+                                    text={title}
+                                    type="input"
+                                    childRef={inputRef}
+                                >
+                                    <Form.Control type="text" placeholder={title} value={title} name="Title" ref={inputRef} onChange={e => setTitle(e.target.value)} />
+                                </Editable>
+                            </h5>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={3} style={{ textAlign: 'left' }}>
+                            <Editable
+                                text={dateTime}
                                 type="input"
                                 childRef={inputRef}
                             >
-                                <input
-                                    ref={inputRef}
-                                    type="text"
-                                    name="start"
-                                    value={title}
-                                    onChange={e => setTitle(e.target.value)}>
-                                </input>
+                                <Form.Control type="datetime-local" placeholder={dateTime} value={dateTime} name="Start" ref={inputRef} onChange={e => setDateTime(e.target.value)} />
                             </Editable>
-                        </h5>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={3} style={{ textAlign: 'left' }}>
-                        <Editable
-                            text={start}
-                            type="input"
-                            childRef={inputRef}
-                        >
-                            <input
-                                ref={inputRef}
-                                type="text"
-                                name="start"
-                                value={start}
-                                onChange={e => setStart(e.target.value)}>
-                            </input>
-                        </Editable>
-                        {/* <p className="mb-0">{event.start.toDateString()}</p> */}
-                        {/* <span>{!event.allDay ? event.start.toLocaleTimeString() + " - " + event.end.toLocaleTimeString() : null}</span> */}
-                        <Editable
-                            text={location}
-                            type="input"
-                            childRef={inputRef}
-                        >
-                            <input
-                                ref={inputRef}
-                                type="text"
-                                name="location"
-                                value={location}
-                                onChange={e => setLocation(e.target.value)}>
-                            </input>
-                        </Editable>
-                        <Editable
-                            text={org}
-                            type="input"
-                            childRef={inputRef}
-                        >
-                            <input
-                                ref={inputRef}
-                                type="text"
-                                name="org"
-                                placeholder={org}
-                                onChange={e => setOrg(e.target.value)}>
-                            </input>
-                        </Editable>
-                        <Editable
-                            text={link}
-                            type="textarea"
-                            childRef={inputRef}
-                        >
-                            <textarea
-                                ref={inputRef}
-                                type="text"
-                                name="link"
-                                value={link}
-                                onChange={e => setLink(e.target.value)}>
-                            </textarea>
-                        </Editable>
-                    </Col>
-                    <Col style={{ textAlign: 'left' }}>
-                    <Editable
-                            text={description}
-                            type="textarea"
-                            childRef={inputRef}
-                        >
-                            <textarea
-                                ref={inputRef}
-                                type="text"
-                                name="link"
-                                value={description}
-                                style={{ overflow: "scroll" }}
-                                cols="90"
-                                rows="5"
-                                onChange={e => setDescription(e.target.value)}>
-                            </textarea>
-                        </Editable>
-                    </Col>
-                </Row>
-            </Card>
+                            {/* <p className="mb-0">{event.start.toDateString()}</p> */}
+                            {/* <span>{!event.allDay ? event.start.toLocaleTimeString() + " - " + event.end.toLocaleTimeString() : null}</span> */}
+                            <Editable
+                                text={location}
+                                type="input"
+                                childRef={inputRef}
+                            >
+                                <Form.Control type="text" placeholder={location} value={location} name="Location" ref={inputRef} onChange={e => setLocation(e.target.value)} />
+                            </Editable>
+                            <Editable
+                                text={org}
+                                type="input"
+                                childRef={inputRef}
+                            >
+                                <Form.Control type="text" placeholder={org} value={org} name="org" ref={inputRef} onChange={e => setOrg(e.target.value)} />
+                            </Editable>
+                            <Editable
+                                text={link}
+                                type="textarea"
+                                childRef={inputRef}
+                            >
+                                <Form.Control type="url" placeholder={link} value={link} name="Link" ref={inputRef} onChange={e => setLink(e.target.value)} />
+                            </Editable>
+                        </Col>
+                        <Col style={{ textAlign: 'left' }}>
+                            <Editable
+                                text={description}
+                                type="url"
+                                childRef={inputRef}
+                            >
+                                <Form.Control type="text" as="textarea" rows={5} placeholder="Description" value={description} name="Description" ref={inputRef} onChange={e => setDescription(e.target.value)} />
+                            </Editable>
+                        </Col>
+                    </Row>
+                </Card>
+            </Form>
         )
     } else {
         return (
