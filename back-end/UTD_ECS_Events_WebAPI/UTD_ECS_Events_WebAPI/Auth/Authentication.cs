86af4193c838edf9806ee530cff24a042eac8727
jiwon <jiwontopper@gmail.com>
2021-02-23 16:42:46 -0600

    Auth

---
+++ back-end/UTD_ECS_Events_WebAPI/UTD_ECS_Events_WebAPI/Auth/Authentication.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UTD_ECS_Events_WebAPI.Auth
+{
+    public class Authentication
+    {
+    }
+}
