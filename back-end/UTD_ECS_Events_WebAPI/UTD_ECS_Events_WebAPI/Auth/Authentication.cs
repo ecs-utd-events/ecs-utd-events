83e22bdbeab4babaaec67b506ffc08b496d412ca
jiwon <jiwontopper@gmail.com>
2021-02-24 10:20:41 -0600

    registration

--- back-end/UTD_ECS_Events_WebAPI/UTD_ECS_Events_WebAPI/Auth/Authentication.cs
+++ back-end/UTD_ECS_Events_WebAPI/UTD_ECS_Events_WebAPI/Auth/Authentication.cs
@@ -3,9 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using FirebaseAdmin;
+using FirebaseAdmin.Auth;
+
+
+
+// Firesharps
+using FireSharp.Config;
+
 namespace UTD_ECS_Events_WebAPI.Auth
 {
     public class Authentication
     {
+
+        /*
+        UserRecordArgs args = new UserRecordArgs()
+        {
+            Email = "utdecsevents@gmail.com",
+            EmailVerified = false,
+            PhoneNumber = "+11234567890",
+            Password = "secretPassword",
+            DisplayName = "John Doe",
+            PhotoUrl = "http://www.example.com/12345678/photo.png",
+            Disabled = false,
+        };
+         UserRecord userRecord = await FirebaseAuth.DefaultInstance.CreateUserAsync(args);
+        // See the UserRecord reference doc for the contents of userRecord.
+        //Console.WriteLine($"Successfully created new user: {userRecord.Uid}");
+        */
     }
 }
