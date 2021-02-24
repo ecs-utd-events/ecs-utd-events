98e0575fe80f8a25a4c7a458b0935d5f51f49b99
mustafa <mustafa.sadriwala@utdallas.edu>
2021-02-24 12:52:52 -0600

    [FE] remove dot in FC list views, remove floating close button in mobile, correct mobile modal navbar

--- front-end/src/components/EventInfoModal.js
+++ front-end/src/components/EventInfoModal.js
@@ -12,14 +12,19 @@ export default function EventInfoModal({ event, handleClose, open }) {
             <div className="modal-wrapper">
                 <AppBar className="modal-appbar">
                     <Toolbar>
-                        <IconButton size="lg" icon={CloseIcon} onClick={handleClose} style={{ backgroundColor: 'var(--primary1)', boxShadow: 'none' }} />
+                        <IconButton
+                            size="lg"
+                            icon={CloseIcon}
+                            onClick={handleClose}
+                            style={{ backgroundColor: 'var(--primary1)', boxShadow: 'none', margin: 0 }}
+                        />
                     </Toolbar>
                 </AppBar>
                 <Container className="modal-container" fluid>
                     <EventInfoContent event={event} mobile />
-                    <div className="close-btn-wrapper">
+                    {/* <div className="close-btn-wrapper">
                         <IconButton size="lg" icon={CloseIcon} onClick={handleClose} style={{ backgroundColor: 'var(--primary1)', boxShadow: "box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);" }} />
-                    </div>
+                    </div> */}
                 </Container>
             </div>
         </Slide>
