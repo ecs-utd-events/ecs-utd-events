386d3e44abda7bd0865d39adbc3bcdfe6703042a
ShannenB <sbarrameda17@gmail.com>
2021-02-25 00:48:30 -0600

    [FE] changing edible event card

--- front-end/src/components/EditableEventCard.js
+++ front-end/src/components/EditableEventCard.js
@@ -9,70 +9,18 @@ import Form from 'react-bootstrap/Form';
 import OrgPageEventCard from './OrgPageEventCard';
 import IconButton from './IconButton';
 import CloseIcon from '@iconify/icons-gg/close';
-import EditIcon from '@iconify/icons-gg/edit-markup';
+
 
 export default function EditableEventCard({ event }) {
     const { register, handleSubmit, watch, errors } = useForm();
     const onSubmit = data => console.log(data);
     const [isEditing, setEditing] = useState(false);
-    // var isEdit = false;
-    if (isEditing) {
-        return (
-            <div className="App">
-            <Card className={"drop-shadow mb-4 "}>
-                <Form>
-                    <Row>
-                        <Col xs={5} style={{ textAlign: 'left' }}>
-                            <Form.Control className="mb-4" type="text" placeholder="Title" name="Title" value={event.title} ref={register} />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={3} style={{ textAlign: 'left' }}>
-                            {/* <p className="mb-0">{event.start.toDateString()}</p> */}
-                            {/* <span>{!event.allDay ? event.start.toLocaleTimeString() + " - " + event.end.toLocaleTimeString() : null}</span> */}
-                            <Form.Control className="mb-4" type="datetime-local" placeholder="Date" value={event.start} name="Date" ref={register} />
-                            <Form.Control className="mb-4" type="text" placeholder="Location" value={event.location} name="Location" ref={register} />
-                            <Form.Control className="mb-4" type="url" placeholder="Link" value={event.link}name="Link" ref={register} />
-                            <Form.Control className="mb-4" type="text" placeholder="Collaborator(s)" name="Collaborator(s)" as="select" value={event.org} custom>
-                                <option>1</option>
-                            </Form.Control>
-                        </Col>
-                        <Col style={{ textAlign: 'left' }}>
-                            <Form.Control type="text" as="textarea" rows={3} placeholder="Description" name="Description" value={event.desecription} ref={register} />
-                            <IconButton icon={CloseIcon} onClick={() => setEditing(!isEditing)}></IconButton>
-                        </Col>
-                    </Row>
-                </Form>
-            </Card>
-            </div>
-        )
-    } else {
-        return (
-            <div>
-                <Card className="drop-shadow mb-4">
-                    <Row>
-                        <Col xs={5} style={{ textAlign: 'left' }}>
-                            <Form.Control className="mb-4" type="text" placeholder="Title" name="Title" value={event.title} ref={register} readOnly/>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={3} style={{ textAlign: 'left' }}>
-                            <Form.Control className="mb-4" type="datetime-local" placeholder="Date" value={event.start} name="Date" ref={register} readOnly/>
-                            <Form.Control className="mb-4" type="text" placeholder="Location" value={event.location} name="Location" ref={register} readOnly/>
-                            <Form.Control className="mb-4" type="url" placeholder="Link" value={event.link}name="Link" ref={register} readonly={true} readOnly />
-                            <Form.Control className="mb-4" type="url" placeholder="Collaborator(s)" value={event.org} name="Org" ref={register} readonly={true} readOnly />
-                        </Col>
-                        <Col style={{ textAlign: 'left' }}>
-                            <Form.Control className="mb-4" type="text" placeholder="Description" value={event.description} name="description" ref={register} readOnly/>
-
-                            <IconButton icon={EditIcon} onClick={() => setEditing(!isEditing)}></IconButton>
-                        </Col>
 
 
-                    </Row>
 
-                </Card>
-            </div>
-        );
-    }
+    return(
+        <OrgPageEventCard event={event} pastEvent={false} isEditable={true}></OrgPageEventCard>
+    );
+   
+    
 }
--- front-end/src/components/OrgPageEventCard.js
+++ front-end/src/components/OrgPageEventCard.js
@@ -1,10 +1,19 @@
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import EditIcon from '@iconify/icons-gg/edit-markup';
+import TrashIcon from '@iconify/icons-gg/trash';
+import IconButton from './IconButton';
+import { ButtonGroup, OverlayTrigger } from 'react-bootstrap';
+import { Tooltip } from 'bootstrap';
 
-export default function OrgPageEventCard({ event, pastEvent }) {
+export default function OrgPageEventCard({ event, pastEvent, isEditable }) {
     const backgroundColor = pastEvent == true ? "org-page-past-event-card" : "";
-    if (event != null) {
+    const renderEditTooltip = (props) => (
+        <Tooltip id="edit-button-tooltip" {...props}>Edit card</Tooltip>
+    );
+
+    if (event && !isEditable) {
         return (
             <Card className={"drop-shadow mb-4 " + backgroundColor}>
                 <Row>
@@ -27,6 +36,38 @@ export default function OrgPageEventCard({ event, pastEvent }) {
                 </Row>
             </Card>
         )
+    } else if(event && isEditable) {
+        return (
+            <Card className={"drop-shadow mb-4"}>
+                <Row>
+                    <Col style={{ textAlign: 'left' }}>
+                        <h5 className="font-weight-bold">{event.title}</h5>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={2} style={{ textAlign: 'left' }}>
+                        {/* <p className="mb-0">{event.start.toDateString()}</p> */}
+                        {/* <span>{!event.allDay ? event.start.toLocaleTimeString() + " - " + event.end.toLocaleTimeString() : null}</span> */}
+                        <p className="mb-0">{event.start}</p>
+                        <p className="mb-0">{event.location}</p>
+                        <p className="mb-0">{event.org}</p>
+                        <a className="mb-0" href={event.link}>More Info</a>
+                    </Col>
+                    <Col style={{ textAlign: 'left' }}>
+                        <p>{event.description}</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={{ span: 10, offset: 10 }}>
+                        <ButtonGroup>
+                        <Tooltip id="edit-button-tooltip" >Edit card</Tooltip>
+                            {/* <OverlayTrigger placement="top" overlay={renderEditTooltip}><IconButton className="mr-2" icon={EditIcon}></IconButton></OverlayTrigger> */}
+                            <IconButton icon={TrashIcon}></IconButton>
+                        </ButtonGroup>
+                    </Col>
+                </Row>
+            </Card>
+        );
     } else {
         return (
             null
