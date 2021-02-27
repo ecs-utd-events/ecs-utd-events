8ee4dc73f38702c2b7aa0ed73d238dba7252c88d
mustafa <mustafa.sadriwala@utdallas.edu>
2021-02-26 20:13:32 -0600

    add authenticated routing for admin portal and dummy loading animation for login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,11 +8,8 @@ import { useEffect } from "react";
 
 import Login from './pages/Login';
 import Home from './pages/Home';
-import CreateEvent from './pages/admin/CreateEvent';
 import OrgProfileRouter from './pages/OrgProfileRouter';
-import EditProfile from "./pages/admin/EditProfile";
-import EditEvents from "./pages/admin/EditEvents";
-import HelpPage from "./pages/admin/HelpPage";
+import AdminRouter from "./pages/admin/AdminRouter";
 import UserProvider from "./providers/UserProvider";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -37,10 +34,7 @@ function App() {
         <Switch>
           <Route path="/org" component={OrgProfileRouter} />
           <Route path="/login" component={Login} />
-          <Route path="/admin/profile" component={EditProfile} />
-          <Route path="/admin/events" component={EditEvents} />
-          <Route path="/admin/help" component={HelpPage} />
-          <Route path="/admin/create" component={CreateEvent} />
+          <Route path="/admin" component={AdminRouter} />
           <Route path="/" component={Home} />
         </Switch>
       </Router>
--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -10,9 +10,10 @@ import { useHistory } from 'react-router-dom';
 import { auth } from '../firebase';
 import { ReactComponent as ECSLogo } from '../assets/utd-ecs-logo-clipped.svg';
 import './../styles/App.css';
+import { Spinner } from 'react-bootstrap';
 
 function getErrorMessage(errorCode) {
-    if(errorCode === 'auth/invalid-email') {
+    if (errorCode === 'auth/invalid-email') {
         return '😴 Please enter a valid email'
     } else if (errorCode === 'auth/user-disabled') {
         return 'Login failed: This username has been disabled. If you believe this to be a mistake then contact the site admin.'
@@ -30,22 +31,31 @@ export default function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [errorCode, setErrorCode] = useState('');
+    const [loading, setLoading] = useState(false);
     const history = useHistory();
 
     const submitHandler = event => {
         event.preventDefault();
         auth.signInWithEmailAndPassword(username, password).then((authCredentials) => {
             setErrorCode('');
-            console.log('Welcome ' + authCredentials.user.email + ' 😎')
-            history.push('/admin/profile');
-        }
-        ).catch((error) => {
+            setLoading(true);
+            setTimeout(() => {
+                console.log('Welcome ' + authCredentials.user.email + ' 😎');
+                setLoading(false);
+                history.push('/admin/profile');
+            }, 1000);
+        }).catch((error) => {
             setErrorCode(error.code);
         })
     }
 
     return (
         <div className="background-accent">
+            {loading &&
+                <div  className="login-spinner-wrapper">
+                    <Spinner animation="border" className="login-spinner" />
+                </div>
+            }
             <div className="login-wrapper">
                 <Container>
                     <Row>
