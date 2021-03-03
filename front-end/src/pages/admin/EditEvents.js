cd61bfcb75e4be76252054b49647891f1ee2e874
sid-devic <sid.devic@gmail.com>
2021-03-03 17:16:41 -0600

    add userContext to shannen's events page

--- front-end/src/pages/admin/EditEvents.js
+++ front-end/src/pages/admin/EditEvents.js
@@ -1,48 +1,29 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState, useContext } from "react";
 import AdminLayout from "../../components/AdminLayout";
 import EditableEventCard from '../../components/EditableEventCard';
 import AddIcon from '@iconify/icons-gg/add';
 import IconButton from '../../components/IconButton';
+import { UserContext } from "../../providers/UserProvider";
 
 const today = new Date()
 let todayStr = today.toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
 
 export default function EditEvents() {
-    let { orgSlug } = useParams();
-    const [organizations, setOrganizations] = useState(null);
-    const [thisOrg, setThisOrg] = useState(null);
-    const [allEvents, setAllEvents] = useState([{
-        id: '123',
-        title: 'A Look into NLP and Research Engineering at Google',
-        start: todayStr + 'T20:00:00',
-        end: todayStr + 'T21:00:00',
-        description: 'As an Education x Research x Industry Event, if you\'re interested in learning how natural language processing and deep learning play several roles in language-oriented products like the Google Assistant or Alexa, come hear from a software engineer at Google discuss the challenges that go with creating these products. We\'ll also be discussing differences between industry and academia, important skills to be an effective software engineer, and what different software-engineering centric career paths in tech might look like. The talk will conclude with an open-ended Q&A forum.',
-        org: 'ACM',
-        location: 'Zoom',
-        link: 'https://cdn.discordapp.com/attachments/714723430079135755/808062875062108180/ACM_Research_Engineering_at_Google_Flyer_1.png'
-    }]);
+    const { org } = useContext(UserContext);
+    // const [organizations, setOrganizations] = useState(null);
+    const [allEvents, setAllEvents] = useState(null);
 
-    const newEvent = {
-        id: '123',
-        title: 'A Look into NLP and Research Engineering at Google',
-        start: todayStr + 'T20:00:00',
-        end: todayStr + 'T21:00:00',
-        description: 'As an Education x Research x Industry Event, if you\'re interested in learning how natural language processing and deep learning play several roles in language-oriented products like the Google Assistant or Alexa, come hear from a software engineer at Google discuss the challenges that go with creating these products. We\'ll also be discussing differences between industry and academia, important skills to be an effective software engineer, and what different software-engineering centric career paths in tech might look like. The talk will conclude with an open-ended Q&A forum.',
-        org: 'ACM',
-        location: 'Zoom',
-        link: ''
-    };
-    // useEffect(() => {
-    //     if (thisOrg != null) {
-    //         fetch((process.env.REACT_APP_SERVER_URL || 'http://localhost:80') + '/api/events/org=' + thisOrg.uId)
-    //             .then(response => response.json())
-    //             .then(data => setAllEvents(data))
-    //             .catch(error => {
-    //                 console.error('There was an error fetching events for this org: ' + thisOrg.name, error);
-    //             });
-    //     }
-    // }, [thisOrg])
+    useEffect(() => {
+        if (org != null) {
+            fetch((process.env.REACT_APP_SERVER_URL || 'http://localhost:80') + '/api/events/org=' + org.uId)
+                .then(response => response.json())
+                .then(data => setAllEvents(data))
+                .catch(error => {
+                    console.error('There was an error fetching events for this org: ' + org.name, error);
+                });
+        }
+    }, [org])
+
     const addEvent = event => {
         event.preventDefault();
         setAllEvents([
@@ -67,22 +48,23 @@ export default function EditEvents() {
 
     const saveEvent = (event, id) => {
         event.preventDefault();
-
     }
-    return (
 
-        <AdminLayout pageName="Events">
-            { allEvents &&
-                allEvents.map(event => {
-
-                    return (<EditableEventCard event={event} isEditable={event.title === ''} deleteEvent={deleteEvent} ></EditableEventCard>)
-
-                })
-            }
-
-
-            <IconButton icon={AddIcon} onClick={addEvent}></IconButton>
-        </AdminLayout>
-
-    )
+    if (org != null) {
+        return (
+            <AdminLayout pageName="Events">
+                { allEvents &&
+                    allEvents.map(event => {
+                        return (
+                            <EditableEventCard event={event} isEditable={event.title === ''} deleteEvent={deleteEvent} ></EditableEventCard>
+                        )
+                    })
+                }
+                <IconButton icon={AddIcon} onClick={addEvent}></IconButton>
+            </AdminLayout>
+        )
+    }
+    else {
+        return null;
+    }
 }
