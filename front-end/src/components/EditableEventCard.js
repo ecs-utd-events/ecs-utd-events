a9f58afb06c6c6cabf777663d3fd4a8acf4a1242
ShannenB <sbarrameda17@gmail.com>
2021-03-03 21:42:52 -0600

    editable event card changes

--- front-end/src/components/EditableEventCard.js
+++ front-end/src/components/EditableEventCard.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
@@ -19,6 +19,7 @@ import CancelIcon from '@iconify/icons-gg/close';
 export default function EditableEventCard({ event, deleteEvent, saveEvent, isEditable }) {
     const { register, handleSubmit, watch, errors } = useForm();
     const [isEditing, setEditing] = useState(isEditable);
+    const [orgs, setOrganizations] = useState(null);
     const onSubmit = data => {
         setEditing(!isEditing);
         console.log(data);
@@ -27,6 +28,32 @@ export default function EditableEventCard({ event, deleteEvent, saveEvent, isEdi
     const validateTime = async () => {
 
     }
+
+    const validateDate = async (date) => {
+        const today = new Date()
+        
+        if(new Date(date) <= today) return false;
+    }
+
+    const cancelEditing = (e) => {
+        deleteEvent(e, event.id);
+        setEditing(!isEditing);
+    };
+
+    useEffect(() => {
+        // GET request for organizations
+        if (orgs != null) {
+            setOrganizations(orgs);
+        }
+        else {
+            fetch((process.env.REACT_APP_SERVER_URL || 'http://localhost:80') + '/api/orgs/all')
+                .then(response => response.json())
+                .then(data => setOrganizations(data))
+                .catch(error => {
+                    console.error('There was an error fetching organizations!', error);
+                });
+        }
+    }, [orgs]);
 
     if (event && !isEditing) {
         return (
@@ -40,13 +67,13 @@ export default function EditableEventCard({ event, deleteEvent, saveEvent, isEdi
                         </Row>
                         <Row>
                             <Col xs={2} style={{ textAlign: 'left' }}>
-                                <p className="mb-0">{!event.allDay ? getFormattedTime(new Date(event.startTime)) + " - " + getFormattedTime(new Date(event.endTime)) : null}</p>
-                                <p className="mb-0">{event.start}</p>
+                                <p className="mb-0">{event.date}</p>
+                                <p className="mb-0">{event.startTime + " - " + event.endTime}</p>
                                 <p className="mb-0">{event.location}</p>
                                 <p className="mb-0">{event.orgs}</p>
-                                <a className="mb-0" href={event.link}>More Info</a>
                             </Col>
                             <Col style={{ textAlign: 'left' }}>
+                                <a className="mb-0" href={event.link}>Link</a>
                                 <p>{event.description}</p>
                             </Col>
                         </Row>
@@ -69,21 +96,19 @@ export default function EditableEventCard({ event, deleteEvent, saveEvent, isEdi
                             <Form.Group controlId="eventTitle">
                                 <Form.Label>Event Title</Form.Label>
                                 <Form.Control type="text" placeholder="Title" name="title" ref={register({ required: true })} defaultValue={event.title} />
-                                {/* { errors.title && <p>required</p>} */}
                             </Form.Group>
                             <Form.Row>
-
                                 <Form.Group as={Col} controlId="date">
                                     <Form.Label>Date</Form.Label>
-                                    <Form.Control type="date" placeholder="Date" name="Date" ref={register({ required: true })} />
+                                    <Form.Control type="date" placeholder="Date" name="Date" ref={register({ required: true, validate: validateDate })} defaultValue={event.date}/>
                                 </Form.Group>
                                 <Form.Group as={Col} controlId="startTime">
                                     <Form.Label>Start time</Form.Label>
-                                    <Form.Control type="time" placeholder="Start Time" name="start" ref={register({ required: true, validate: validateTime })} />
+                                    <Form.Control type="time" placeholder="Start Time" name="startTime" ref={register({ required: true, validate: validateTime })} defaultValue={event.startTime}/>
                                 </Form.Group>
                                 <Form.Group as={Col} controlId="endTime">
                                     <Form.Label>End time</Form.Label>
-                                    <Form.Control type="time" placeholder="End Time" name="end" ref={register({ required: true, validate: validateTime })} />
+                                    <Form.Control type="time" placeholder="End Time" name="endTime" ref={register({ required: true, validate: validateTime })} defaultValue={event.endTime}/>
                                 </Form.Group>
                             </Form.Row>
                             <Form.Row>
@@ -92,7 +117,6 @@ export default function EditableEventCard({ event, deleteEvent, saveEvent, isEdi
                                         <Form.Label>Location</Form.Label>
                                         <Form.Control type="text" placeholder="Location" name="location" ref={register({ required: true })} defaultValue={event.location} />
                                     </Form.Group>
-
                                     <Form.Group controlId="orgs">
                                         <Form.Label>Collaborator(s)</Form.Label>
                                         <Form.Control type="text" placeholder="Collaborator(s)" name="orgs" as="select" custom>
@@ -109,13 +133,12 @@ export default function EditableEventCard({ event, deleteEvent, saveEvent, isEdi
                                         <Form.Label>Description</Form.Label>
                                         <Form.Control type="text" as="textarea" rows={4} placeholder="Description" name="description" defaultValue={event.description} />
                                     </Form.Group>
-
                                 </Col>
                             </Form.Row>
                             <Form.Row>
                                 <Col md={{ span: 10, offset: 10 }}>
                                     <IconButton className="mr-2" icon={SaveIcon} type="submit" onClick={handleSubmit(onSubmit)}></IconButton>
-                                    <IconButton className="mr-2" icon={CancelIcon} onClick={() => setEditing(!isEditing)}></IconButton>
+                                    <IconButton className="mr-2" icon={CancelIcon} onClick={cancelEditing}></IconButton>
                                     {/* <IconButton icon={TrashIcon} onClick={(e) => deleteEvent(e, event.id)}></IconButton> */}
                                 </Col>
                             </Form.Row>
--- front-end/src/pages/admin/EditEvents.js
+++ front-end/src/pages/admin/EditEvents.js
@@ -5,34 +5,30 @@ import EditableEventCard from '../../components/EditableEventCard';
 import AddIcon from '@iconify/icons-gg/add';
 import IconButton from '../../components/IconButton';
 
-const today = new Date()
-let todayStr = today.toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
+
 
 export default function EditEvents() {
     let { orgSlug } = useParams();
     const [organizations, setOrganizations] = useState(null);
     const [thisOrg, setThisOrg] = useState(null);
+    const today = new Date()
+    let todayStr = today.toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
+
     const [allEvents, setAllEvents] = useState([{
         id: '123',
         title: 'A Look into NLP and Research Engineering at Google',
-        start: todayStr + 'T20:00:00',
-        end: todayStr + 'T21:00:00',
+        date: todayStr,
+        startTime: today.toTimeString().substr(0,5),
+        endTime: today.toTimeString().substr(0,5),
         description: 'As an Education x Research x Industry Event, if you\'re interested in learning how natural language processing and deep learning play several roles in language-oriented products like the Google Assistant or Alexa, come hear from a software engineer at Google discuss the challenges that go with creating these products. We\'ll also be discussing differences between industry and academia, important skills to be an effective software engineer, and what different software-engineering centric career paths in tech might look like. The talk will conclude with an open-ended Q&A forum.',
         org: 'ACM',
         location: 'Zoom',
         link: 'https://cdn.discordapp.com/attachments/714723430079135755/808062875062108180/ACM_Research_Engineering_at_Google_Flyer_1.png'
     }]);
 
-    const newEvent = {
-        id: '123',
-        title: 'A Look into NLP and Research Engineering at Google',
-        start: todayStr + 'T20:00:00',
-        end: todayStr + 'T21:00:00',
-        description: 'As an Education x Research x Industry Event, if you\'re interested in learning how natural language processing and deep learning play several roles in language-oriented products like the Google Assistant or Alexa, come hear from a software engineer at Google discuss the challenges that go with creating these products. We\'ll also be discussing differences between industry and academia, important skills to be an effective software engineer, and what different software-engineering centric career paths in tech might look like. The talk will conclude with an open-ended Q&A forum.',
-        org: 'ACM',
-        location: 'Zoom',
-        link: ''
-    };
+    
+    
+ 
     // useEffect(() => {
     //     if (thisOrg != null) {
     //         fetch((process.env.REACT_APP_SERVER_URL || 'http://localhost:80') + '/api/events/org=' + thisOrg.uId)
@@ -50,8 +46,9 @@ export default function EditEvents() {
             {
                 id: '',
                 title: '',
-                start: todayStr + '',
-                end: todayStr + 'T21:00:00',
+                date: todayStr,
+                startTime: today.toTimeString().substr(0,5),
+                endTime: today.toTimeString().substr(0,5),
                 description: '',
                 org: '',
                 location: '',
@@ -74,12 +71,9 @@ export default function EditEvents() {
         <AdminLayout pageName="Events">
             { allEvents &&
                 allEvents.map(event => {
-
                     return (<EditableEventCard event={event} isEditable={event.title === ''} deleteEvent={deleteEvent} ></EditableEventCard>)
-
                 })
             }
-
 
             <IconButton icon={AddIcon} onClick={addEvent}></IconButton>
         </AdminLayout>
