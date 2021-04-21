afa597bdebed52496af8e5240e576c79438a695e
Shannen Barrameda <sbarrameda17@gmail.com>
2021-04-21 12:40:43 -0500

    created help page

--- front-end/src/pages/admin/HelpPage.js
+++ front-end/src/pages/admin/HelpPage.js
@@ -1,9 +1,35 @@
 import AdminLayout from "../../components/AdminLayout";
+import Accordion from 'react-bootstrap/Accordion';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Icon from '@iconify/react';
+import DropdownIcon from '@iconify/icons-mdi/arrow-down-drop-circle-outline';
+import { questions } from '../../constants/FrequentlyAskedQuestions';
 
-export default function HelpPage(){
-    return(
+export default function HelpPage() {
+    return (
         <AdminLayout pageName="Help">
-            <h1>Help Page</h1>
+            <Container>
+            <h1 className="my-3" style={{textAlign:'center'}}>FAQs</h1>
+            {questions.map(question => {
+                return (
+                    <Accordion>
+                        <Card className="mt-2">
+                            <Card.Header>
+                                <Accordion.Toggle as={Button} variant="light" eventKey="0" >
+                                <Icon className="mr-3 mb-1" icon={DropdownIcon} />
+                                    {question.question}
+                                </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="0">
+                                <Card.Body>{question.answer}</Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                    </Accordion>
+                )
+            })}
+            </Container>
         </AdminLayout>
-    )
+    );
 }
