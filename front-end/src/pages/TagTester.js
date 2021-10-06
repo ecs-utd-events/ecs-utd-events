10b8eb110ec546b23a40fef2cdf135287adf6700
Arunima Mittra <arunima1820@gmail.com>
2021-10-05 23:35:25 -0500

    added formatting to search bar

--- front-end/src/pages/TagTester.js
+++ front-end/src/pages/TagTester.js
@@ -2,6 +2,8 @@
 import { useState, useEffect, useContext, useMemo } from 'react';
 import Chip from '@material-ui/core/Chip';
 import * as React from 'react';
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/esm/Col';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 
@@ -23,23 +25,46 @@ export default function TagTester() {
     const sorted = tags.sort((a, b) => a.category.toString() < b.category.toString() ? 1 : -1)
 
     return (
-        <div>
-            <h1>Hello World</h1>
-           
-            <Autocomplete
+        <div>          
+            {/* <Autocomplete
                 multiple
                 id="tags-standard"
                 options={sorted}
                 groupBy={(option) => option.category.toString()}
                 getOptionLabel={(option) => option.acronym.toString()}
                 renderInput={(params) => (
-                <TextField
-                    {...params}
-                    variant="standard"
-                    placeholder="Tags"
+                    <TextField
+                        {...params}
+                        variant="standard"
+                        placeholder="Tags"
+                    /> 
+                )}
+            /> */}
+
+        <Row className="home-page-filters mx-1 mt-2">
+            {/* organizations filter */}
+            <Col xs={12} sm={4} className="d-flex align-items-end pl-2 pr-0">
+            <Autocomplete
+                name="tags"
+                options={sorted}
+                groupBy={(option) => option.category.toString()}
+                getOptionLabel={(option) => option.acronym.toString()}
+                //onChange={(e, value, _) => setOrgFilterValue(getOrgIds(value))}
+                loading={tags.length === 0}
+                renderInput={(params) => 
+                    <TextField 
+                        style={{}} 
+                        {...params} 
+                        margin="small" 
+                    />}
+                getOptionSelected={(option, value) => option.acronym === value.acronym}
+                limitTags={1}
+                
+                // clearOnEscape
+                multiple
             />
-        )}
-      />
+            </Col>
+        </Row>
         </div>
     )
 }
