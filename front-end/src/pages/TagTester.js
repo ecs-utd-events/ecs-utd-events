441a3e2a1feb2d2097c0a9bda5aa0c2c8cdd0cc7
Arunima Mittra <arunima1820@gmail.com>
2021-10-07 17:17:47 -0500

    tester works too

--- front-end/src/pages/TagTester.js
+++ front-end/src/pages/TagTester.js
@@ -6,41 +6,19 @@ import Row from 'react-bootstrap/esm/Row';
 import Col from 'react-bootstrap/esm/Col';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
-
-
+import { apiProvider } from '../providers/Provider';
 
 export default function TagTester() {
     const [tags, setTags] = useState([])
 
     useEffect(() => {
-        //fetch((process.env.REACT_APP_SERVER_URL || 'http://localhost:80') + '/api/tags/all')
-        fetch('http://localhost:80' + '/api/tags/all')
-            .then(response => response.json())
-            .then(data => setTags(data))
-            .catch(error => {
-                console.error('There was an error fetching tags!', error);
-            });
+        apiProvider.getAll('tags', setTags)
     }, [])
 
     const sorted = tags.sort((a, b) => a.category.toString() < b.category.toString() ? 1 : -1)
 
     return (
-        <div>          
-            {/* <Autocomplete
-                multiple
-                id="tags-standard"
-                options={sorted}
-                groupBy={(option) => option.category.toString()}
-                getOptionLabel={(option) => option.acronym.toString()}
-                renderInput={(params) => (
-                    <TextField
-                        {...params}
-                        variant="standard"
-                        placeholder="Tags"
-                    /> 
-                )}
-            /> */}
-
+        <div>
         <Row className="home-page-filters mx-1 mt-2">
             {/* organizations filter */}
             <Col xs={12} sm={4} className="d-flex align-items-end pl-2 pr-0">
